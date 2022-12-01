cce6f2bbf024873737e7f1c17c50e3ac66b17d4c
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-12-01 19:21:33 -0300

    Arruma endpoint POST/categories :bug:

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
-app.use('/category', categoryRouter);
+app.use('/categories', categoryRouter);
 
 // ...
 
