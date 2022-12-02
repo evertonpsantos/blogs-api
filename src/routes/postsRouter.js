a017d1d6b454d7823b6b0d64a7674b8a364e45d7
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-12-02 13:09:17 -0300

    Arrumando GET /post :bug:

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { validateToken } = require('../auth/validateJWT');
-const { createNewBlogPost, getAllPosts } = require('../controllers/post.controller');
-const { validateNewPost } = require('../middlewares/validations');
+const { getAllPosts } = require('../controllers/post.controller');
+// const { validateNewPost } = require('../middlewares/validations');
 
 const postsRouter = express.Router();
 
-postsRouter.post('/', validateToken, validateNewPost, createNewBlogPost);
+// postsRouter.post('/', validateToken, validateNewPost, createNewBlogPost);
 
 postsRouter.get('/', validateToken, getAllPosts);
 
